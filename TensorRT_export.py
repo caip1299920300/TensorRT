b4d888ae1fe37ea2fd51d52032ce409d4162b8bb
wucaipeng <wucaipeng@kfb.cn>
2023-08-15 14:24:34 +0800

    添加了备注以及tensor的量化

--- TensorRT_export.py
+++ TensorRT_export.py
@@ -20,41 +20,45 @@ import torch,time
 import contextlib
 import inspect
 from pathlib import Path
+# 返回文件/目录的大小（MB）
 def file_size(path):
-    # Return file/dir size (MB)
+    # Return file/dir size (MB) 
     mb = 1 << 20  # bytes to MiB (1024 ** 2)
     path =  Path(path)
     if path.is_file():
-        return path.stat().st_size / mb
+        return path.stat().st_size / mb # 返回文件大小
     elif path.is_dir():
-        return sum(f.stat().st_size for f in path.glob('**/*') if f.is_file()) / mb
+        return sum(f.stat().st_size for f in path.glob('**/*') if f.is_file()) / mb # 返回目录中所有文件的大小之和
     else:
-        return 0.0
+        return 0.0  # 路径既不是文件也不是目录，返回0.0
 
+# 获取函数func()的默认参数
 def get_default_args(func):
     # Get func() default arguments
     signature = inspect.signature(func)
     return {k: v.default for k, v in signature.parameters.items() if v.default is not inspect.Parameter.empty}
 
+# Profile类。用法：@Profile()装饰器或'with Profile():'上下文管理器
 class Profile(contextlib.ContextDecorator):
     # YOLOv5 Profile class. Usage: @Profile() decorator or 'with Profile():' context manager
     def __init__(self, t=0.0):
-        self.t = t
-        self.cuda = torch.cuda.is_available()
+        self.t = t # 累计时间
+        self.cuda = torch.cuda.is_available() # 检查是否可用CUDA
 
     def __enter__(self):
-        self.start = self.time()
+        self.start = self.time() # 记录开始时间
         return self
 
     def __exit__(self, type, value, traceback):
-        self.dt = self.time() - self.start  # delta-time
-        self.t += self.dt  # accumulate dt
+        self.dt = self.time() - self.start  # 计算时间差 delta-time
+        self.t += self.dt  # 累加时间差
 
     def time(self):
         if self.cuda:
-            torch.cuda.synchronize()
-        return time.time()
+            torch.cuda.synchronize() # 等待CUDA操作完成
+        return time.time() # 返回当前时间戳
 
+# 导出装饰器，即 @try_export
 def try_export(inner_func):
     # YOLOv5 export decorator, i..e @try_export
     inner_args = get_default_args(inner_func)
@@ -62,17 +66,17 @@ def try_export(inner_func):
     def outer_func(*args, **kwargs):
         try:
             with Profile() as dt:
-                f, model = inner_func(*args, **kwargs)
-            print(f'export success ✅ {dt.t:.1f}s, saved as {f} ({file_size(f):.1f} MB)')
-            return f, model
+                f, model = inner_func(*args, **kwargs) # 执行内部函数
+            print(f'export success ✅ {dt.t:.1f}s, saved as {f} ({file_size(f):.1f} MB)') # 打印导出成功的消息
+            return f, model # 返回导出的结果
         except Exception as e:
-            print(f'export failure ❌ {dt.t:.1f}s: {e}')
-            return None, None
+            print(f'export failure ❌ {dt.t:.1f}s: {e}') # 打印导出失败的消息
+            return None, None # 返回空结果
 
     return outer_func
 
 @try_export
-def export_engine(onnx_model_path,engine_path, workspace=4):
+def export_engine(onnx_model_path,engine_path, workspace=4,half=2):
     # 创建TensorRT的Logger对象
     logger = trt.Logger(trt.Logger.INFO)
     # 创建TensorRT的Builder对象
@@ -89,6 +93,11 @@ def export_engine(onnx_model_path,engine_path, workspace=4):
     #if not parser.parse(model.SerializeToString()):
     if not parser.parse_from_file(str(onnx_model_path)):
         raise RuntimeError(f'failed to load ONNX file: {onnx_model_path}')
+    # 判断是否使用float16精度
+    if builder.platform_has_fast_fp16 and half==1:
+        config.set_flag(trt.BuilderFlag.FP16)
+    elif builder.platform_has_fast_int8 and half==2:
+        config.set_flag(trt.BuilderFlag.INT8)
     # 构建并优化TensorRT引擎
     engine = builder.build_engine(network, config)
     if engine is None:
